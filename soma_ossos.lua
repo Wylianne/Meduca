b6db961fdf1ba0e8a516666d33aa87e92f8d2cf7
Wylianne <wyliannec@gmail.com>
2017-05-02 11:17:09 -0300

    cadeado nas fases fixes #2

--- soma_ossos.lua
+++ soma_ossos.lua
@@ -42,31 +42,62 @@ function geraFases()
             cont = 0
         end
 
-        local faseBack = display.newRoundedRect(display.contentCenterX * 1.05, 30, 70,90,12)
-        faseBack:setFillColor(0.5,0.6,1, 0.5)
-        faseBack.y = yFaseB
-        faseBack.x = iniFase
-        SceneGroup:insert(faseBack)
+        if (i <= 1) then  -- verifica no bd até que fase esta desbloqueado
+            local faseBack = display.newRoundedRect(display.contentCenterX * 1.05, 30, 70,90,12)
+            faseBack:setFillColor(0.5,0.6,1, 0.5)
+            faseBack.y = yFaseB
+            faseBack.x = iniFase
+            SceneGroup:insert(faseBack)
 
-        local faseFront = display.newRoundedRect(display.contentCenterX * 1.05, 30, 70,70,12)
-        faseFront:setFillColor(0.5,0.6,1, 0.5)
-        faseFront.y = yFaseF
-        faseFront.x = iniFase
-        faseFront.strokeWidth = 3
-        faseFront:setStrokeColor( 1, 1, 0 )
-        SceneGroup:insert(faseFront)
+            local faseFront = display.newRoundedRect(display.contentCenterX * 1.05, 30, 70,70,12)
+            faseFront:setFillColor(0.5,0.6,1, 0.5)
+            faseFront.y = yFaseF
+            faseFront.x = iniFase
+            faseFront.strokeWidth = 3
+            faseFront:setStrokeColor( 1, 1, 0 )
+            SceneGroup:insert(faseFront)
 
-        local numFase = display.newText(i, 150, yFaseF, native.systemFont, 40 )
-        numFase.x = display.contentCenterX
 
-        if cont == 0 then
-            numFase.x = display.contentCenterX -100
-        elseif cont == 1 then
+            local numFase = display.newText(i, 150, yFaseF, native.systemFont, 40 )
             numFase.x = display.contentCenterX
-        elseif cont == 2 then
-            numFase.x = display.contentCenterX + 100
+
+            if cont == 0 then
+                numFase.x = display.contentCenterX -100
+            elseif cont == 1 then
+                numFase.x = display.contentCenterX
+            elseif cont == 2 then
+                numFase.x = display.contentCenterX + 100
+            end
+            SceneGroup:insert(numFase)
+        elseif (i > 1) then
+
+            local faseFront = display.newRoundedRect(display.contentCenterX * 1.05, 30, 70,70,12)
+            faseFront:setFillColor(0.66,0.66,0.66)
+            faseFront.y = yFaseF
+            faseFront.x = iniFase
+            faseFront.strokeWidth = 3
+            faseFront:setStrokeColor(0.66,0.66,0.66)
+            SceneGroup:insert(faseFront)
+
+
+            local cadeado = display.newImageRect("img_jogos/cadeado.png", 40, 40)
+            cadeado.x = display.contentCenterX
+
+
+            if cont == 0 then
+                cadeado.x = display.contentCenterX -100
+                cadeado.y = yFaseF
+            elseif cont == 1 then
+                cadeado.x = display.contentCenterX
+                cadeado.y = yFaseF
+            elseif cont == 2 then
+                cadeado.x = display.contentCenterX + 100
+                cadeado.y = yFaseF
+            end
+            SceneGroup:insert(cadeado)
         end
-        SceneGroup:insert(numFase)
+
+
 
         iniFase = iniFase + 100
         cont = cont + 1
