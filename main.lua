0e303d7d2b3b0c3552f2d3379eb2c75c9bcc62f5
juniorpires <juniorpiresupe@gmail.com>
2017-05-03 23:41:08 -0300

    iniciado fase 1 das cruzadinhas

--- main.lua
+++ main.lua
@@ -8,6 +8,6 @@
 
 
 local composer = require( "composer" )
-composer.gotoScene("login")
+composer.gotoScene("cruzadas_f1")
 --composer.gotoScene("menuPrincipal")
 --composer.gotoScene("quebra_ossos")
